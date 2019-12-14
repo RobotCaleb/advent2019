f02e6ff909cd8f7c6d1220edae36584741fdef58
Caleb Anderson <robotrising@gmail.com>
2019-12-14 11:00:36 -0700

    stubbed out opcodes 5-8, refactor parameter modes

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,5 +1,10 @@
 use simpleuserinput::simple_user_input::get_input;
 
+enum ParameterMode {
+    Position,
+    Immediate,
+}
+
 pub struct IntCode {
     pub state: Vec<i32>,
 }
@@ -17,60 +22,82 @@ impl IntCode {
         let mut pc: usize = 0;
         loop {
             let opcode = self.state[pc];
-            let mut step = 0;
             let opi = opcode % 100;
             let a = ((opcode / 10000) % 10) as u32;
             let b = ((opcode / 1000) % 10) as u32;
             let c = ((opcode / 100) % 10) as u32;
-            let param_modes = vec![c, b, a];
+            let param_modes = vec![self.get_mode(c), self.get_mode(b), self.get_mode(a)];
             match opi {
                 1 => {
                     // add from first two parameters, store at 3rd
                     self.add(
-                        self.get_at(self.state[pc + 1], param_modes[0]),
-                        self.get_at(self.state[pc + 2], param_modes[1]),
+                        self.get_at(self.state[pc + 1], &param_modes[0]),
+                        self.get_at(self.state[pc + 2], &param_modes[1]),
                         self.state[pc + 3] as usize,
                     );
-                    step = 4;
+                    pc += 4;
                 }
                 2 => {
                     // multiply from first two parameters, store at 3rd
                     self.mul(
-                        self.get_at(self.state[pc + 1], param_modes[0]),
-                        self.get_at(self.state[pc + 2], param_modes[1]),
+                        self.get_at(self.state[pc + 1], &param_modes[0]),
+                        self.get_at(self.state[pc + 2], &param_modes[1]),
                         self.state[pc + 3] as usize,
                     );
-                    step = 4;
+                    pc += 4;
                 }
                 3 => {
                     // get input, store at 2nd
                     let index = self.state[pc + 1] as usize;
                     let input = get_input("Input: ").parse::<i32>().unwrap();
                     self.state[index] = input;
-                    step = 2;
+                    pc += 2;
                 }
                 4 => {
                     // output value at 2nd
                     let index = self.state[pc + 1];
-                    let val = self.get_at(index, param_modes[0]);
+                    let val = self.get_at(index, &param_modes[0]);
                     println!("{}", val);
-                    step = 2;
+                    pc += 2;
                 }
+                // 5 => {
+                //     // jump if true
+                //     let index = self.state[pc + 1];
+                //     let val = self.get_at(index, &param_modes[0]);
+                //     println!("{}", val);
+                //     step = 2;
+                // }
+                // 6 => {
+                //     // jump if false
+                //     let index = self.state[pc + 1];
+                //     let val = self.get_at(index, &param_modes[0]);
+                //     println!("{}", val);
+                //     step = 2;
+                // }
+                // 7 => {
+                //     // less than
+                //     let index = self.state[pc + 1];
+                //     let val = self.get_at(index, &param_modes[0]);
+                //     println!("{}", val);
+                //     step = 2;
+                // }
+                // 8 => {
+                //     // equals
+                //     let index = self.state[pc + 1];
+                //     let val = self.get_at(index, &param_modes[0]);
+                //     println!("{}", val);
+                //     step = 2;
+                // }
                 99 => return,
                 _ => println!("Error at index {} found {}", pc, opcode),
             }
-            pc += step;
         }
     }
 
-    fn get_at(&self, at: i32, mode: u32) -> i32 {
+    fn get_at(&self, at: i32, mode: &ParameterMode) -> i32 {
         match mode {
-            0 => self.state[at as usize],
-            1 => at as i32,
-            _ => {
-                println!("Unexpected mode: {}", mode);
-                std::process::exit(1);
-            }
+            ParameterMode::Position => self.state[at as usize],
+            ParameterMode::Immediate => at as i32,
         }
     }
 
@@ -80,5 +107,16 @@ impl IntCode {
 
     fn mul(&mut self, a: i32, b: i32, index: usize) {
         self.state[index] = a * b;
+    }
+
+    fn get_mode(&self, mode: u32) -> ParameterMode {
+        match mode {
+            0 => ParameterMode::Position,
+            1 => ParameterMode::Immediate,
+            _ => {
+                println!("Invalid parameter mode: {}", mode);
+                std::process::exit(1);
+            }
+        }
     }
 }
